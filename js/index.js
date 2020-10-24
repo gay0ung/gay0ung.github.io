215a409aedc6aa1586f5d253b4f11c038d096d39
gay0ung <6and_end9@naver.com>
2020-10-24 23:13:27 +0900

    update 4일차.
    닫기 버튼
    header
    section 부분 해결 해야함.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
-
-
+const pageWrap = document.querySelector('.page-wrap');
+const buttons = document.querySelectorAll('.h-nav > button')
+const sections = document.querySelectorAll('.main-inner > section');
+const scrollIcon = document.querySelector('.scroll');
+// const section = document.querySelector('section');
+const closeBtn = document.querySelectorAll('.close-btn');
+console.log(closeBtn);
+headerNavBtn()
 function headerNavBtn(){
-  let buttons = document.querySelectorAll('.h-nav > button')
-  let sections = document.querySelectorAll('.main-inner > section');
-
   for(let i = 0; i < buttons.length; i++){
-
     buttons[i].onclick = ((clickNum)=>{
       return (e)=>{
         e.preventDefault();
 
-        buttons.forEach((el, idx) => {
+        Array.from(buttons).map((el, idx) => {
           el.classList.remove("on");
           sections[idx].classList.remove("on")
         })
@@ -18,43 +20,36 @@ function headerNavBtn(){
         buttons[i].classList.add("on")
         sections[i].classList.add("on")
       }
+
     })(i)
   }
   checkedUl()
 };
-headerNavBtn()
+
 
 
 function checkedUl(id){
   const ul = document.querySelectorAll("ul.sec"),
         li = document.querySelectorAll('.list > li');
-
-  const scrollIcon = document.querySelector('.scroll');
-
-
   
-  ul.forEach((el,idx) => {
+  Array.from(ul).map((el,idx) => {
     if(el.classList.contains("list")){
       let liLen = el.children.length;
       let lis = el.children;
 
-      if (liLen === 1) {
-        el.children[0].style.height = "344px";
-        scrollIcon.style.opacity = '0';
-        console.log(idx);
-      }
+      // 길이가 1이고 li에 클릭이 없어야 한다.
+ if (!pageWrap.classList.contains('click') && liLen === 1) {
+        // el.children[0].style.height = "400px";
+        // scrollIcon.style.opacity = '0';
+      } 
 
       listClickHandler(lis)
     }
   })  
-
 }
 
-
-
+// 리스트 클릭했을때 상세내용 보이게 하기
 function listClickHandler(list){
-  const pageWrap = document.querySelector('.page-wrap');
-
   const CLICK = "click",
         ACTIVE = "active",
         HIDE = "hide"
@@ -62,32 +57,25 @@ function listClickHandler(list){
   for(let i = 0; i < list.length; i++){
     list[i].onclick = ((targetNum)=>{
       return (e)=>{
-        if (!e.target.classList.contains('sec-img')) return;
+        let target = e.target.classList;
+        console.log(e.target);
+        if (!target.contains('sec-img')) return
 
-        if (e.target.classList.contains('sec-img')){
-          let secContainer = e.target.parentElement;
-          let closeBtn = secContainer.previousElementSibling;
+          Array.from(list).forEach(el => {
+              el.className = HIDE
+          });
 
-          list[i].className = CLICK;
-          if (list[i].className === CLICK){
-            pageWrap.classList.add(CLICK)
-          }
-        }
-
+          list[targetNum].className = CLICK;
+          pageWrap.classList.add(CLICK);
+          // scrollIcon.style.opacity = "0";
+          closeBtn[targetNum].classList.add(ACTIVE)
+          console.log(closeBtn[targetNum]);
+        
       }
-    
     })(i)
   }
-  // let secContainer = document.querySelector('.sec-container')
-  // console.log(secContainer);
-  // for(let i = 0; i < list.length; i++){
+}
 
-  //   list[i].addEventListener('click',(e)=>{
+function clsoeBtn(){
 
-
-  //     if (e.target.classList.contains('sec-img')){
-  //       console.log('click');
-  //     }
-  //   })
-  // }
 }
